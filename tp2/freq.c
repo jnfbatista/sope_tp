b909201f99057b9223772264bfdac9dbf9a00ac4
batista <batista@batista-laptop.localdomain>
2009-12-08 21:51:29 +0000

    Lế do fifo e imprime

--- tp2/freq.c
+++ tp2/freq.c
@@ -3,17 +3,19 @@
 #include <signal.h>
 #include <string.h>
 #include <fcntl.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #define FREQ_PIPE "/tmp/sosh.canal"
-
+/*
 struct Letter {
-	char[1];
+	char letter[1];
 	int frequency;
 };
 
 struct freq_table{
-	Letter* letters[];
-};
+	struct Letter* letters[];
+};*/
 
 int init_pipe() {
 	if( mkfifo(FREQ_PIPE, 0600) == -1 ) {
@@ -22,6 +24,16 @@ int init_pipe() {
 }
 
 int main(int argc, char *argv[], char* envp[]) {
+	int fd;
+	char buf[256];
+
+	fd = open(FREQ_PIPE, O_RDONLY);
+	if (fd != -1) {
+		read(fd, buf, 256);
+		
+	}
+	
+	printf("%s\n", buf);
 
 	return 0;
 }
