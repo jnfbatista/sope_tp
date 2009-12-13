ce6012697f1ac000744ab060283c0229b3fcb882
root <root@batista-laptop.localdomain>
2009-12-13 02:03:42 +0000

    adicionados ficheiros em falta

---
+++ tp2/freq_cmds.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void freq_count(char txt[], int letters[]) {
+	int i = 0;
+	int c;
+
+	while(txt[i] != 0) {
+		c = (int)txt[i];
+		if (c > 64) {
+			if(c < 91) {
+				letters[c-65]++;
+				printf("%d - %c\n", c, txt[i]);
+			} else if (c > 96) {
+				if (c < 123) {
+					letters[c-97]++;
+					printf("%d - %c\n", c, txt[i]);
+				}
+
+			}
+		}
+		i++;
+	}
+} 
+
+
+void freq_diplay(int letters[]) {
+	int i =0, l_size = 24;
+	while(i < l_size) {
+		printf("%c\t%d\n", i+97, letters[i]);
+		i++;
+	}
+}
---
+++ tp2/freq_cmds.h
@@ -0,0 +1,10 @@
+#ifndef _FREQ_CMDS_H_
+#define _FREQ_CMDS_H_
+
+#include <stdio.h>
+
+void freq_count(char txt[], int letter[]);
+
+void freq_diplay(int letters[]);
+
+#endif 
